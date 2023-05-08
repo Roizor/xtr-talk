34bc38257db82b65faaa3d5d51f7de0b03c3ad7e
Roizor <aidenjs9@icloud.com>
2023-05-07 20:20:41 -0500

    add ;

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main( ) {
 
   char buf[RANDOM_STRING_LENGTH + 1];
   for (size_t i = 0; i < RANDOM_STRING_LENGTH; i++) {
-    buf[i] = "abcdefghijklmnopqrstuvwxyz:1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ€¢$¢¥.!,/<>?"[rand() %76];
+    buf[i] = "abcdefghijklmnopqrstuvwxyz:;1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ€¢$¢¥.!,/<>?"[rand() %77];
   }
   buf[RANDOM_STRING_LENGTH] = '\0';
 
