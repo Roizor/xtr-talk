058cd876490d007b2b5bc01a7bc34b27d9d675a9
Roizor <aidenjs9@icloud.com>
2023-05-20 11:06:48 -0500

    Uploaded by script

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sodium.h>
 #include "xtr.c"
 
 int main( ) {
+
+  if (sodium_init() < 0) {
+        /* panic! the library couldn't be initialized, it is not safe to use */
+        return 1; 
+    }
+
   // Not even i'm sure how this code works
 
-  srand(time(NULL));
+  srand(time(NULL) * 0.5 * time(NULL));
 
   char buf[xtr_talk_length + 1];
   for (size_t i = 0; i < xtr_talk_length; i++) {
